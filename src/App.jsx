3ff078334c2c2708449608fde63b8d564ad97b6e
JyotiInHex <paragjyoti43d@gmail.com>
2024-09-29 21:37:46 +0530

    App Upload 29 Sep 24

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black w-full h-[100vh] text-white flex items-center justify-center text-6xl font-semibold'>Arogyam</div>
+  )
+}
+
+export default App
